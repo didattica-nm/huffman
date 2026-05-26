819bfe059c7d2caad9c333056721debda3884d6b
Nickolausen <74073441+Nickolausen@users.noreply.github.com>
2026-05-26 22:44:09 +0000

    Deploying to gh-pages from @ NickolausenTeaching/huffman@1095964519a0177dfba9ead13a3c50098ab6b43c 🚀

---
+++ files/Huffman.cs
@@ -0,0 +1,23 @@
+class HuffmanEncoder
+{
+    /// <summary>
+    /// Funzione di Encoding che, preso in input il contenuto di un file di testo, 
+    /// mi restituisce una stringa contenente la rispettiva codifica secondo 
+    /// l'algoritmo di Huffman
+    /// </summary>
+    /// <param name="content">Contenuto da codificare</param>
+    /// <returns>Il contenuto `content` codificato.</returns>
+    public static string Encode(string content)
+    {
+        // 1) Calcolo la tabella delle frequenze di ciascun carattere
+        var frequencyTable = BuildFrequencyTable(content);
+        // 2) Costruisco l'albero di Huffman a partire da quelle frequenze
+        var huffmanTree = BuildHuffmanTree(frequencyTable);
+        // 3) Calcolo i codici per ciascun carattere
+        var codes = CalculateHuffmanCodes(huffmanTree);
+
+        // 4) TODO: Dati i codici appena calcolati, completare la codifica!
+        // [...]
+        // return [...] 
+    }
+}
